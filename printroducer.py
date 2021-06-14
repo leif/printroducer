2b585b354bea46793716f2da5b2efc9097c240a8
Leif Ryge <leif@synthesize.us>
2021-06-14 21:59:31 +0200

    misc improvements

--- printroducer.py
+++ printroducer.py
@@ -4,6 +4,9 @@
 This creates pairs of easy-to-type 128-bit shared secrets which you can print
 on paper and use for authenticating with SMP or PANDA.
 
+A blank space is provided for additional entropy to be added manually at the
+time of use.
+
 usage:
 cat /usr/share/dict/words | python printroducer.py > printroductions.html
 """
@@ -21,36 +24,30 @@ def printroducer(howmany=50):
     words = set()
     for line in sys.stdin.readlines():
         for word in line.split():
-            if not REJECT.search( word ):
-                words.add( word )
+            if not REJECT.search( word ) and len(word) < 8:
+                words.add( word.lower() )
     words = list(words)
     word_count = len(words)
     bpw = math.log(word_count, 2)
     words_needed = int( math.ceil( BITS / bpw ) )
-    stderr( "Loaded %s words; %s words * %.3f bits per word = %s bits\n" % (word_count, words_needed, bpw, BITS) )
-    print """<html><style>
+    stderr( "Loaded %s words; %s words * %.1f bits per word = %.1f bits (> %s bits)\n" % (word_count, words_needed, bpw, words_needed*bpw, BITS) )
+    print("""<html><style>
 div {
-    margin:0.5em; padding:0;
+    margin:1em; padding:0;
     page-break-inside: avoid;
 }
 pre {
-    font-size:10px;
+    font-size:12pt;
     float:left; width:45%;
     white-space: pre-wrap;
     text-align:center;
-    margin:1em 0; padding:1em;
+    margin:1em; padding:1em;
     border:1px solid black;
-    border-left:0;
-}
-pre:first-child {
-    border-right: 1px dashed black;
-    border-left:1px solid black;
-    clear:all;
 }
 </style>
-"""
+""")
     for i in range(howmany):
-        secret = " ".join( choice(words) for j in range(words_needed) )
-        print "<div><pre>%s</pre><pre>%s</pre></div>" % (secret, secret)
+        secret = " ".join( [ choice(words) for j in range(words_needed) ] +["_"*10] )
+        print("<div><pre>%s</pre><pre>%s</pre></div>" % (secret, secret))
 
 if __name__ == "__main__": printroducer(*sys.argv[1:])
